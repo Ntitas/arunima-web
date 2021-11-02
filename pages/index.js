e720dffbfb171d7e26c6f58289c188dc36eb4d28
Pritha Saha <sahapritha819@gmail.com>
2021-11-02 15:00:56 +0600

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Col, Row, Container } from "react-bootstrap";
+import { Col, Row, Container, Card } from "react-bootstrap";
 
 import MySlider from "../components/MySlider";
 import Widget from "../components/widget";
@@ -9,7 +9,18 @@ import bannerPic from "../public/images/banner.jpg";
 import logo from "../public/images/logo.png";
 import valuesPic from "../public/images/values.png";
 import certPic from "../public/images/certifications.png";
-
+import vf from "../public/images/vf.png";
+import gap from "../public/images/gap.png";
+import george from "../public/images/george.jpg";
+import Walmart from "../public/images/Walmart.png";
+import matalan from "../public/images/matalan.png";
+import primark from "../public/images/primark.jpg";
+import target from "../public/images/target.jpg";
+import belk from "../public/images/belk.png";
+import meijer from "../public/images/meijer.png";
+import nygard2 from "../public/images/nygard2.png";
+import CSR from "../public/images/CSR.png";
+import Kontoor from "../public/images/Kontoor.jpg";
 export default function Home() {
   return (
     <Container className="mt-3 min-vh-100">
@@ -60,7 +71,7 @@ export default function Home() {
         <Image src={valuesPic} alt="Arunima" className="rounded shadow-sm" />
       </Row>
 
-      <h3 className="text-center fw-bold p-3">Videos</h3>
+      {/* <h3 className="text-center fw-bold p-3">Videos</h3>
 
       <Row className="mb-3">
         <Col className="rounded shadow-sm p-3 me-3 bg-white">
@@ -85,32 +96,146 @@ export default function Home() {
             allowFullScreen="true"
           ></iframe>
         </Col>
-      </Row>
+      </Row> */}
 
       <h3 className="text-center fw-bold p-3">Our Clients</h3>
 
       <Row>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+            }}
+          >
+            <Image src={Kontoor} alt="Kontoor" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+              paddingLeft: "10px",
+              paddingRight: "10px",
+              paddingBottom: "10px",
+              paddingTop: "10px",
+            }}
+          >
+            <Image src={vf} alt="VF" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+              paddingLeft: "10px",
+              paddingRight: "10px",
+              paddingBottom: "10px",
+              paddingTop: "10px",
+            }}
+          >
+            <Image src={Walmart} alt="WALMART" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+              // paddingLeft: "10px",
+              // paddingRight: "10px",
+              // paddingBottom: "10px",
+              // paddingTop: "10px",
+            }}
+          >
+            <Image src={matalan} alt="Matalan" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+            }}
+          >
+            <Image src={primark} alt="Primark" className="rounded shadow-sm" />
+          </Card>
+        </Col>
       </Row>
 
       <Row>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
-        <Col className="rounded shadow-sm m-3 p-5 bg-white"></Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+            }}
+          >
+            <Image src={george} alt="George" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+            }}
+          >
+            <Image src={target} alt="Target" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+            }}
+          >
+            <Image src={belk} alt="Belk" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+              paddingLeft: "20px",
+              paddingRight: "20px",
+              // paddingBottom: "10px",
+              paddingTop: "30px",
+            }}
+          >
+            <Image src={meijer} alt="Meijer" className="rounded shadow-sm" />
+          </Card>
+        </Col>
+        <Col className="rounded shadow-sm m-1 p-2 bg-white">
+          <Card
+            style={{
+              width: "240px",
+              height: "150px",
+              // paddingLeft: "20px",
+              // paddingRight: "20px",
+              // paddingBottom: "10px",
+              // paddingTop: "30px",
+            }}
+          >
+            <Image src={nygard2} alt="Nygard2+" className="rounded shadow-sm" />
+          </Card>
+        </Col>
       </Row>
 
       <h3 className="text-center fw-bold p-3">Certifications</h3>
 
       <Row className="rounded shadow-sm mb-3 p-5 bg-white">
         <Image src={certPic} alt="Arunima" className="rounded shadow-sm" />
+      </Row>
+
+      <h3 className="text-center fw-bold p-3">CSR Program</h3>
+
+      <Row className="rounded shadow-sm mb-3 p-5 bg-white">
+        <Image src={CSR} alt="CSR" className="rounded shadow-sm" />
       </Row>
     </Container>
   );
