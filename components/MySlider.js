9f3bb65f4f6cc305097af4e36b3b8763ff1bf0ca
Pritha Saha <sahapritha819@gmail.com>
2021-11-02 16:35:16 +0600

    slider updated

--- components/MySlider.js
+++ components/MySlider.js
@@ -1,20 +1,49 @@
 import Image from "next/image";
 import { Carousel } from "react-bootstrap";
 import sliderImage from "../public/images/banner.jpg";
+import sliderImage1 from "../public/images/banner4.jpg";
+// import sliderImage2 from "../public/images/banner3.jpg";
 
 const MySlider = () => {
   return (
     <Carousel fade>
       <Carousel.Item>
         <Image
-          className="d-block w-100 rounded shadow"
+          className="d-block rounded shadow"
           src={sliderImage}
           alt="First slide"
         />
+
         <Carousel.Caption>
           <h1>Welcome to Arunima Group</h1>
         </Carousel.Caption>
       </Carousel.Item>
+      <Carousel.Item>
+        <Image
+          className="d-block   rounded shadow"
+          src={sliderImage1}
+          alt="Secound slide"
+          width={1400}
+          height={655}
+        />
+
+        <Carousel.Caption>
+          <h1></h1>
+        </Carousel.Caption>
+      </Carousel.Item>
+      {/* <Carousel.Item>
+        <Image
+          className="d-block   rounded shadow"
+          src={sliderImage2}
+          alt="Secound slide"
+          width={1400}
+          height={655}
+        />
+
+        <Carousel.Caption>
+          <h1>Welcome to Arunima Group</h1>
+        </Carousel.Caption>
+      </Carousel.Item> */}
     </Carousel>
   );
 };
