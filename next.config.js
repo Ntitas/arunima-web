a0390159ea81073bb7be8dd0fd3a61bd03565dd0
Bappi Datta <bappi.sust@gmail.com>
2025-01-12 19:04:51 +0600

    Upgrade next & react version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: 'akamai',
-    path: '',
-  },
+    unoptimized: true,
+  }
 }
+
+module.exports = nextConfig
